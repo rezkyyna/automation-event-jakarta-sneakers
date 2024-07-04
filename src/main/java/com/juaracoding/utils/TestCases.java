2b08d0282c5f61d586a7aba29912dab53005be82
Fikrimahmudi24 <fikrimahmudi24@outlook.com>
2024-07-04 10:14:47 +0700

    Login Completed

--- src/main/java/com/juaracoding/utils/TestCases.java
+++ src/main/java/com/juaracoding/utils/TestCases.java
@@ -3,10 +3,17 @@ package com.juaracoding.utils;
 public enum TestCases {
 
     //Register
-    T1("Show Activity Menu");
+    T1("User Access the invalid URL"),
+    T2("User access the valid URL"),
+    T3("User leaves the username and password field blank"),
+    T4("User leaves the password field blank"),
+    T5("User" +
+            " leaves the username field blank"),
+    T6("user input username and password correctly"),
+    ;
 
 
-    private String testCaseName;
+    private final String testCaseName;
     TestCases(String value){
         testCaseName = value;
     }
--- src/test/java/com/juaracoding/RunnerTest.java
+++ src/test/java/com/juaracoding/RunnerTest.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = {"src/main/resources/features/Register.feature"},
+        features = {"src/main/resources/features/01Login.feature"},
         glue = {"com.juaracoding"},
         plugin = {"pretty", "html:target/cucumber-report.html", "json:target/cucumber.json"}
 )
